ba16c2ed5d88fbbd3e4d66ae031b10d06353cf0b
yohannis adane <164261628+YohannisAdane@users.noreply.github.com>
2024-07-09 13:09:56 -0700

    banner

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import Banner from "../../components/Banner/Banner";
 import Footer from "../../components/Footer/Footer";
-
-function Home() {
+const Home = () => {
   return (
-    <>
+    <div>
       <Header />
+      <Banner />
       <Footer />
-    </>
+    </div>
   );
-}
+};
 
 export default Home;
